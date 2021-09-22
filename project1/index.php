8cea69a42cb0689e804761cf0a57ae546589ec7a
Yoonho Kim <ykim27@wccnet.edu>
2021-09-22 16:04:54 -0400

    testupdate

--- project1/index.php
+++ project1/index.php
@@ -1,4 +1,26 @@
-<?php
-echo "this is the index page for project1, More code here!";
-echo "test line 2"
-?>
+<head></head>
+<label>First Name: <input type="text" name="first_name" size="15"></label>
+<body>
+Hello, today is <?php echo date('l, F jS, Y'); ?>.
+<table border="1">
+ <tr>
+ <td>Cell 1</td>
+ <td>Cell 2</td>
+ <td>TEST</td>
+ </tr>
+ <tr>
+ <td>row 2 cell 1</td>
+ <td>row 2 cell 2</td>
+ <td>row 2 cell 3</td>
+ </tr>
+</table>
+<label>Title <input type="text" size="5" name="somename"></label>
+<h1>Glossary</h1>
+<dl>
+ <dt>XHTML</dt>
+ <dd>Extensible Hypertext Markup Language, the structural markup of a web page</dd>
+ <dt>CSS</dt>
+ <dd>Cascading Style Sheets, which determines the presentation of a web page</dd>
+</dl>
+</body>
+</html>
