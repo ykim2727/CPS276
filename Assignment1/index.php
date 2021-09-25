8d072780fd3cd938a3bcc7821d72d20e58af5f6c
Yoonho Kim <ykim27@wccnet.edu>
2021-09-25 13:47:49 -0400

    oup6180

--- Assignment1/index.php
+++ Assignment1/index.php
@@ -8,11 +8,65 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
-    <title>Form Test</title>
+    <title>Form Project</title>
   </head>
   <body>
-    <h1>Form Test</h1>
+    <div class = "container">
+          <form name="project form" method = "post" action = "#" id="project form" class="form">
+                  <!-- First name and last name boxes on the first row -->
+                  <div class = "form-group row">
+                    <div class = "col">  
+                      <label for = "fname">First Name</label>
+                      <input type = "text" class = "form-control" name ="firstname" id = "fname">
+                    </div>
+                    <div class = "col">
+                      <label for = "lname">Last Name</label>
+                      <input type = "text" class = "form-control" name ="lastname" id = "lname">
+                    </div>
+                  </div>
+                
+                  <!-- Address box on 1 row-->
+                  <div class = "form-group">
+                      <label for = "address">Address</label>
+                      <input type = "text" class = "form-control" name = "address" id = "add">
+                  </div>
 
-    
+
+                  <!-- city, state, and zip on 1 row-->
+                <div class = "row">
+                  <div class = "form-group col-md-6">
+                      <label for = "cty">City</label>
+                      <input type = "text" class = "form-control" name = "city" id = "cty">
+                  </div>
+                  <div class="form-group col-md-4">
+                    <label for="statenames">State</label>
+                      <select id="statenames" name="state" class="form-select">
+                        <option value="illinois">Illinois</option>  
+                        <option value="indiana">Indiana</option>
+                        <option value="michigan" selected>Michigan</option>
+                        <option value="ohio">Ohio</option>
+                        <option value="wisconsin">Wisconsin</option>
+                        
+                      </select>
+                </div>
+                <div class="form-group col-md-2">
+                    <label for="zip">Zip</label>
+                    <input type="text" class="form-control" name="zipcode" id="zip">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="gender" id="optionmale" value="male">
+                    <label class="form-check-label" for="optionmale">Male</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="gender" id="optionfemale" value="female">
+                    <label class="form-check-label" for="optionfemale">Female</label>
+                </div>   
+            </div>
+            <button type="submit" class="btn btn-primary">Register</button>
+        </form>
+    </div>
   </body>
 </html>
