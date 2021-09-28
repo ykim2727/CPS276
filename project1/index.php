8e0b6cb23eaf5d6ac20e4700b646168a85cd52cc
Yoonho Kim <ykim27@wccnet.edu>
2021-09-28 19:45:11 -0400

    9.28notes

--- project1/index.php
+++ project1/index.php
@@ -1,7 +1,37 @@
 <?php
+    $i = 0;
+    $output = "";
+    while ($i < 10){
+        $output .= $i."--";
+        $i++;
+    }
+    $str = "Programming in PHP is cool";
+    $arr = explode(" ",$str);
+    echo "<pre>";
+    print_r($arr);
+    echo "</pre>";
+
 $firstName = "justin";
 $lastName = "kim";
 $age = "25";
+$ageTwo = 25;
+
+
+if($firstName == $lastName){
+    echo "equal";
+}
+else echo "not equal";
+
+if ($age == $ageTwo){
+    echo " EQUAL";
+}
+
+$string = <<<HTML
+<p>This is a heredoc multi-line string with "quotes", the double quotes render because we are using 
+the heredoc, where both 'single' and "double quotes" render</p>;
+HTML;
+
+echo $string;
 
 ?>
 
@@ -15,6 +45,7 @@ $age = "25";
 </head>
 <body>
     <p>
+    <?php echo $output;?>
         My name is <?php echo $firstName." ".$lastName?>. I am <?php echo $age ?> years old
         <label>Test</label>
         <input type = "text" class = "form-control" name ="firstname" id = "fname">
