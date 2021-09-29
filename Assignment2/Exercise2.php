a20b5b5964e07b2cccd9753045f69dfbcff9a330
Yoonho Kim <ykim27@wccnet.edu>
2021-09-29 18:36:16 -0400

    update

--- Assignment2/Exercise2.php
+++ Assignment2/Exercise2.php
@@ -1,12 +1,57 @@
+<?php
+
+$title = "Exercise 2";
+$header = "My Web Page";
+$name = "Justin (Yoonho) Kim";
+$paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus feugiat mollis dolor at bibendum. In congue maximus ligula, ut faucibus mi accumsan at. Vestibulum sagittis tortor eget dui ultricies, a vulputate lacus faucibus. Fusce aliquet bibendum erat, sed bibendum eros cursus eu. Nulla at neque rhoncus, ultricies odio at, accumsan elit. Proin in turpis eu leo dapibus pulvinar. Vivamus viverra massa ut enim fringilla ultricies. Donec in enim blandit, iaculis nulla quis, egestas elit. Nullam ut enim id erat bibendum finibus nec ac eros. Nulla malesuada ex facilisis ultrices rhoncus. Nullam in euismod nisl. Donec pulvinar ex sit amet aliquet egestas.";
+$footer = "My Web Page 2021";
+
+for ($i=0; $i<=1; $i++){
+    $paragraph .= "<br>";
+    $paragraph .= "<br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus feugiat mollis dolor at bibendum. In congue maximus ligula, ut faucibus mi accumsan at. Vestibulum sagittis tortor eget dui ultricies, a vulputate lacus faucibus. Fusce aliquet bibendum erat, sed bibendum eros cursus eu. Nulla at neque rhoncus, ultricies odio at, accumsan elit. Proin in trpis eu leo dapibus pulvinar. Vivamus viverra massa ut enim fringilla ultricies. Donec in enim blandit, iaculis nulla quis, egestas elit. Nullam ut enim id erat bibendum finibus nec ac eros. Nulla malesuada ex facilisis ultrices rhoncus. Nullam in euismod nisl. Donec pulvinar ex sit amet aliquet egestas.";
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercise 2</title>
+	<meta charset="UTF-8">
+	<title><?php echo $title;?></title>
+	<style>
+		* {margin: 0; padding: 0;}
+		body {font: 120%/1.5 sans-serif;}
+		#wrapper {width: 1000px; margin: 0 auto; border: 1px solid black;}
+		header {background: green; height: 150px; padding: 20px;}
+		header h1 {color: white;}
+		main {padding: 10px;}
+		main h2 {margin: 15px 0;}
+		main p {margin-bottom: 15px;}
+		footer {background: #eee; padding: 10px 0; text-align: center}
+		footer p {font-size: .8em;}
+	</style>
 </head>
 <body>
-    
+	<div id="wrapper">
+		<header>
+			<h1><?php 
+					echo $header;
+				?></h1>
+		</header>
+		<main>
+			<h2>My name is <?php echo $name; ?></h2>
+			<p><?php 
+					echo $paragraph;
+				?></p>
+			
+		</main>
+		<footer>
+			<p><?php 
+					echo $footer; 
+				?></p>
+		</footer>
+	</div>
+	
 </body>
 </html>
--- Assignment2/Exercise3.php
+++ Assignment2/Exercise3.php
@@ -1,3 +1,32 @@
+<?php
+
+$rows = [];
+$cells = [];
+
+for ($i=1;$i<=15;$i++){
+    array_push($rows,$i);
+}
+
+for ($o=1;$o<=5;$o++){
+    array_push($cells,$o);
+}
+
+$table = "<table border='1'>";
+
+foreach ($rows as $row){
+    $table .= "<tr>";
+    foreach($cells as $cell){
+        $table .= "<td>Row $row Cell $cell</td>";
+    }
+    $table .= "</tr>";
+}
+
+$table .= "</table>";
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +36,8 @@
     <title>Exercise 3</title>
 </head>
 <body>
-    
+    <?php
+        echo $table;
+    ?>
 </body>
 </html>
