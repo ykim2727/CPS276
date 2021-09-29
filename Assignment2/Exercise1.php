fd3362886eec8c290768f5327ea49763d26d6b01
Yoonho Kim <ykim27@wccnet.edu>
2021-09-29 17:35:01 -0400

    part1

--- Assignment2/Exercise1.php
+++ Assignment2/Exercise1.php
@@ -1,3 +1,22 @@
+<?php>
+
+$mainList = [1,2,3,4];
+$subList = [1,2,3,4,5];
+$output = "<ul>";
+
+foreach($mainList as $nums){
+    $output .= "<li>$nums<ul>";
+        foreach($subList as $subNums){
+            $output .= "<li>$subNums</li>";
+        }
+        $output .= "</ul></li>";
+}
+
+$output .= "</ul>";
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +26,10 @@
     <title>Exercise 1</title>
 </head>
 <body>
+    <?php 
     
+    echo $output; 
+    
+    ?>
 </body>
 </html>
