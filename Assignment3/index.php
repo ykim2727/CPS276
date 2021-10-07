7d23b600361b18f670fdbd19bbb393143480bde5
Yoonho Kim <ykim27@wccnet.edu>
2021-10-07 12:09:31 -0400

    test

---
+++ Assignment3/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assignment 3 - Calculator</title>
+</head>
+    <body>
+        <?php
+            require_once "Calculator.php";
+            $Calculator = new Calculator();
+            echo $Calculator->calc("/", 10, 0); 
+            echo $Calculator->calc("*", 10, 2); 
+            echo $Calculator->calc("/", 10, 2); 
+            echo $Calculator->calc("-", 10, 2);
+            echo $Calculator->calc("+", 10, 2); 
+            echo $Calculator->calc("*", 10);
+            echo $Calculator->calc(10);
+        ?>
+    </body>
+</html>
